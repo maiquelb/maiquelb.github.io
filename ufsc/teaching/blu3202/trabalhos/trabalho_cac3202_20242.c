ca56eb439ad2aec3ecefb9d94d07433702fea51d
maiquelb <maiquelb@gmail.com>
2024-11-04 11:15:56 -0300

    update trabalho cac3202 2024-2

--- ufsc/teaching/blu3202/trabalhos/trabalho_cac3202_20242.c
+++ ufsc/teaching/blu3202/trabalhos/trabalho_cac3202_20242.c
@@ -79,14 +79,32 @@ void* gerar_valores_de_sensores(void* arg) {
 // Função que simula a leitura de valores de sensores
 void* ler_valores_de_sensores(void* arg) {
     Visita visita;     
-    while (thread_status==1) {
+    while (thread_status == 1) {
         // Espera até receber o sinal da thread de geração de valores
         pthread_mutex_lock(&mutex);
+
+        // Verifica se o programa ainda deve continuar antes de esperar pelo sinal
+        if (thread_status == 0) {
+            pthread_mutex_unlock(&mutex);
+            break;
+        }
+
         pthread_cond_wait(&cond, &mutex);
         
-        printf("%f\n", distancia_obstaculo()); 
-        visita = ultima_visita();
-        printf("(%d,%d) - %02d:%02d:%02d\n",visita.coordenada.x, visita.coordenada.y, visita.hora->tm_hour, visita.hora->tm_min, visita.hora->tm_sec);
+        if (thread_status == 0) {
+            pthread_mutex_unlock(&mutex);
+            break;
+        }
+        
+        //*** As três linhas abaixo ilustram a leitura de sensores. Descomentar para testar 
+        //printf("Distância para o obstáculo mais próximo: %f\n", distancia_obstaculo());
+        //Visita visita = ultima_visita();
+        //printf("(%d,%d) - %02d:%02d:%02d\n",visita.coordenada.x, visita.coordenada.y, visita.hora->tm_hour, visita.hora->tm_min, visita.hora->tm_sec);
+        
+        /**** Implementar a leitura dos sensores aqui ****/
+        
+        
+        /**** Finalizar a leitura dos sensores aqui  ****/
         
         pthread_mutex_unlock(&mutex);
     }
@@ -94,20 +112,26 @@ void* ler_valores_de_sensores(void* arg) {
 }
 
 
+//**** Implementar todas as funções e estruturas de dados aqui ****/
+
+
+
+//**** 	 ****/
+
 
 int main() {
-    pthread_t thread_id;
+    pthread_t thread_gerar, thread_ler;
     Visita visita; 
     int opcao=0;
     
     // Criação da thread que simula geração de valores de sensores
-    if (pthread_create(&thread_id, NULL, gerar_valores_de_sensores, NULL) != 0) {
+    if (pthread_create(&thread_gerar, NULL, gerar_valores_de_sensores, NULL) != 0) {
         perror("Erro ao criar thread");
         return 1;
     }
     
     // Criação da thread que lê de valores de sensores simulados
-    if (pthread_create(&thread_id, NULL, ler_valores_de_sensores, NULL) != 0) {
+    if (pthread_create(&thread_ler, NULL, ler_valores_de_sensores, NULL) != 0) {
         perror("Erro ao criar thread");
         return 1;
     }
@@ -119,6 +143,9 @@ int main() {
         printf("2. Imprimir os dados do último ponto visitado\n");
         printf("3. Imprimir a distância média para o obstáculo mais próximo nas primeiras 'x' leituras\n");
         printf("4. Imprimir a distância média para o obstáculo mais próximo nas últimas 'x' leituras\n");
+        printf("5. Imprimir o horário da primeira visita a um ponto.\n");
+        printf("6. Imprimir o a distância entre o último ponto ponto visitado e o ponto mais distante das coordenadas (0, 0)\n");
+        printf("6. Imprimir o a distância entre o último ponto ponto visitado e o ponto mais próximo das coordenadas (0, 0)\n");
         printf("9. Encerrar o programa\n");
         
 
@@ -132,8 +159,14 @@ int main() {
               visita = ultima_visita();
               printf("(%d,%d) - %02d:%02d:%02d\n",visita.coordenada.x, visita.coordenada.y, visita.hora->tm_hour, visita.hora->tm_min, visita.hora->tm_sec);
               break;
+           /**** Implementar as opções 3 a 7 aqui ****/   
+           
+           /**** Fim da implementação das opções 3 a 7  ****/
            case 9: 
-              thread_status=0; 
+              pthread_mutex_lock(&mutex);
+              thread_status = 0;
+              pthread_cond_broadcast(&cond); // Sinalizar todas as threads
+              pthread_mutex_unlock(&mutex);
               break;     
            default:
               printf("Opção inválida\n");
@@ -141,26 +174,13 @@ int main() {
 
         
         
-    
-        //printf("Pressione ENTER para ver o valor do contador...\n");
-        //getchar();  // Aguarda o usuário pressionar ENTER
-        //printf("%f -(%d,%d) - %02d:%02d:%02d",distancia, v.coordenada.x, v.coordenada.y, v.hora->tm_hour, v.hora->tm_min, v.hora->tm_sec);
-        
-        /*printf("Valor do contador: %d\n", contador);
-        // Exiba a hora atual
-    printf("Hora atual: %02d:%02d:%02d\n",
-           info_tempo->tm_hour,
-           info_tempo->tm_min,
-           info_tempo->tm_sec);
-
-        */
-        //printf("1. Distância até o obstáculo.\n");
         
         
     }
 
-    // Espera a thread
-    pthread_join(thread_id, NULL);
+    // Espera as threads finaizarem
+    pthread_join(thread_gerar, NULL);
+    pthread_join(thread_ler, NULL);
 
     return 0;
 }

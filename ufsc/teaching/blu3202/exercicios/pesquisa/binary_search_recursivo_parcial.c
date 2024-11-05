02244ade0a4d9b5afb4cdb74bc8caa67d409666f
maiquelb <maiquelb@gmail.com>
2024-11-05 15:41:37 -0300

    add ufsc pdf files

---
+++ ufsc/teaching/blu3202/exercicios/pesquisa/binary_search_recursivo_parcial.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int binary_search(int inicio, int fim, int *v, int x){
+
+}
+
+int main(){
+   int i, n, vetor[10] = {1,2,3,4,5,6,7,8,9,10};
+   printf("Digite o numero para localizar: ");
+   scanf("%d", &n);
+   printf("Resultado da busca: %d\n", binary_search(0,10,vetor,n));
+}

1ab037f401c3b8a78689fbb62632d25c58ea4b97
maiquelb <maiquelb@gmail.com>
2024-11-07 14:22:55 -0300

    add ufsc pdf files

---
+++ ufsc/teaching/blu3202/exercicios/ordenacao/selectionSort_parcial.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+void ordena(int *v, int n){
+
+}
+
+int main(){
+   int v[10] = {9,8,7,6,5,4,3,2,1,0};
+   int i;
+   ordena(v,10);
+   for(i=0;i<10;i++){
+      printf("%d ", v[i]);
+   }
+
+}
